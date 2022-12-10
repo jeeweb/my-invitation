42d7a7536ac5f8d90d831bbd4ec6d3448978c5a3
jeeweb <52396673+jeeweb@users.noreply.github.com>
2022-12-10 10:58:59 +0900

    텍스트 출력

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,23 @@ const makeBox = document.getElementById("makeBox");
 const resultBox = document.getElementById("resultBox");
 const makeBtn = document.getElementById("makeBtn");
 const fileMain = document.getElementById("fileMain");
+const iptTitle = document.getElementById("iptTitle");
+const iptTxt = document.getElementById("iptTxt");
+const cardTitle = document.getElementById("cardTitle");
+const cardTxt = document.getElementById("cardTxt");
 
+// 변수에 잠깐 저장해서 출력하는 방식, 차후 변경
+function getTxt() {
+  let title = iptTitle.value;
+  let txt = iptTxt.value;
+  cardTitle.innerText = title;
+  cardTxt.innerText = txt;
+}
+
+/*
+  현재 로컬주소로 가져와서 preview 형식으로 보여주게 함
+  차후 변경
+*/
 function getMainPhoto(img) {
   if (img.files && img.files[0]) {
     const reader = new FileReader();
@@ -19,6 +35,7 @@ function handleMakeBtn(e) {
   makeBox.classList.remove("show");
   resultBox.classList.add("show");
   getMainPhoto(fileMain);
+  getTxt();
 }
 
 makeBtn.addEventListener("click", handleMakeBtn);
